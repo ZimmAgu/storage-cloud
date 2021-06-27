189f337e90ca3a7f1f44e35b48c615e4577b4bae
Zim Agu <zimmagu@gmail.com>
2021-06-26 17:51:05 -0700

    Added the set current user function

--- src/myComponents/authorization.js
+++ src/myComponents/authorization.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from 'react'
+import React, {useState, useContext, useEffect} from 'react'
 import { auth } from '../firebase'
 
 const authorizationContext = React.createContext() 
@@ -7,16 +7,29 @@ const authorizationContext = React.createContext()
 const useAuthContext = useContext(authorizationContext)
 
 
-function signUserUp (email, password) { // Will be imported to signUp.js and used to create a user
-    return auth.createUserWithEmailAndPassword(email, password)
-}
-
-
 function AuthorizationProvider( {descendants} ) { // Whatever props are passed to this function will inherit the Context value
     const [currentUser, setCurrentUser] = useState('') // There is no current user until someone signs up of signs in
-
-    const user = {currentUser} // This provider will use this to pass the state of the current user to all descendants
     
+    function signUserUp (email, password) { // Will be imported to signUp.js and used to create a user
+        return auth.createUserWithEmailAndPassword(email, password)
+    }
+
+
+    useEffect(() => { 
+        const cleanUp = auth.onAuthStateChanged(userState => { // When a user logs in, the current user will be set to the logged in user. When a user logs out, the current user will be set to null
+            setCurrentUser(userState)
+        })
+
+        return cleanUp // Cleans up the side effect of every state change
+    }, []) // The current user should only be set on mount instead of every time the function is rendered
+
+    
+    const user = {
+        currentUser,
+        signUserUp
+    } // This provider will use this to pass the state of the current user to all descendants
+
+
     return (
         <>
             <AuthorizationContext.Provider value={user}>
