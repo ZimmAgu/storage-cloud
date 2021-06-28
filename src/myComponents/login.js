5d1cd4d6dc3acad9aa6f4f4f613b99327428b3d4
Zim Agu <zimmagu@gmail.com>
2021-06-28 15:31:14 -0700

    Forgot password link added

--- src/myComponents/login.js
+++ src/myComponents/login.js
@@ -55,6 +55,9 @@ function LoginForm () {
 
                                 <Button className="w-100 " type="submit" disabled={pageIsLoading}>Log In</Button>
                             </Form>
+                            <div className="w-100 text-center mt-2">
+                                <Link to="./forgotpassword">Forgot Password?</Link>
+                            </div>
                         </Card.Body>
                     </Card>
                     <div className="w-100 text-center mt-2">
