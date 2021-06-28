52f79286148f44d8d6c6e80a2edec4b9071e2c20
Zim Agu <zimmagu@gmail.com>
2021-06-27 19:24:30 -0700

    Sign up route complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import AuthorizationProvider from './authorization';
+import SignUpForm from './myComponents/signUp';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
 
 
 function App () {
-  return (
-    <>
-      <AuthorizationProvider />
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <Switch>
+                    <Route
+                        path="/signup"
+                        render={() => (
+                            <AuthorizationProvider>
+                                <SignUpForm />
+                            </AuthorizationProvider>
+                         )}
+                    />
+                </Switch>
+            </Router>
+        </>
+    );
 }
 
 export default App;
