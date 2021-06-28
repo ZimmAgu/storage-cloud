edf3b9a6576fca3e464fc65b094570ea12a18695
Zim Agu <zimmagu@gmail.com>
2021-06-28 13:26:29 -0700

    Homepage now hase a private route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AuthorizationProvider from './firebaseAuth/authorization';
 import SignUpForm from './myComponents/signUp';
 import LoginForm from './myComponents/login';
 import Homepage from './myComponents/homepage';
+import PrivateRoute from './myComponents/privateRoute';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 
@@ -12,7 +13,7 @@ function App () {
             <Router>
                 <Switch>
                     <AuthorizationProvider>
-                        <Route exact path="/" component={Homepage} />
+                        <PrivateRoute exact path="/" component={Homepage} />
 
                         <Route path="/signup" component={SignUpForm} />
 
---
+++ src/myComponents/privateRoute.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {useAuthContext} from '../firebaseAuth/authorization'
+import { Route, Redirect } from 'react-router';
+
+function PrivateRoute ({ component: Component, ...rest }) {
+    const {currentUser} = useAuthContext();
+    return (
+        <>
+           <Route {...rest}
+                render={props => {
+                    return currentUser ? <Component props /> : <Redirect to='./login'/>
+                }}
+           /> 
+        </>
+    )
+}
+
+export default PrivateRoute
