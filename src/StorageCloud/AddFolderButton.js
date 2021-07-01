475f9a60ee75b9f2420de5be7592c64b86c56b65
Zim Agu <zimmagu@gmail.com>
2021-06-30 22:21:29 -0700

    Add folder now has some submission events. But it still does not add a folder

--- src/StorageCloud/AddFolderButton.js
+++ src/StorageCloud/AddFolderButton.js
@@ -1,13 +1,25 @@
-import {React, useState} from 'react'
+import {React, useState, useRef} from 'react'
 import {Button, Form, Modal} from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFolderPlus, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 function AddFolderButton() {
-    const [show, setShow] = useState();
+    const [show, setShow] = useState(false); // Used to show and hide the modal
+    const [folderName, setFolderName] = useState('') // Sets the name of the folder
+
 
     const handleShow = () => setShow(true);
     const handleClose = () => setShow(false);
+
+    function handleInputChange (event) { // Changes the value of the create folder input every time the user enters in a character
+        setFolderName(event.target.value)
+    }
+
+    function handleSubmission (event) {
+        event.preventDefault()
+        setFolderName('')
+        handleClose()
+    }
     
     return (
         <>
@@ -16,24 +28,30 @@ function AddFolderButton() {
             </Button>
 
             <Modal show={show} onHide={handleClose}>
-                <Modal.Header>
-                     New Folder
-                     <FontAwesomeIcon icon={faTimes} onClick={handleClose} style={{cursor:'pointer'}} size='lg'/>
-                </Modal.Header>
+                <Form onSubmit={handleSubmission}> 
+                    <Modal.Header>
+                        New Folder
+                        <FontAwesomeIcon icon={faTimes} onClick={handleClose} style={{cursor:'pointer'}} size='lg'/>
+                    </Modal.Header>
 
-                <Modal.Body>
-                    <Form>       
-                        <Form.Group>
-                            <Form.Control type="text" placeholder="Enter the name of your folder"></Form.Control>
-                        </Form.Group>
-                    </Form>    
-                </Modal.Body>
+                    <Modal.Body>   
+                            <Form.Group>
+                                <Form.Control 
+                                    type="text" 
+                                    placeholder="Enter the name of your folder" 
+                                    value={folderName} 
+                                    onChange={handleInputChange} 
+                                    required 
+                                />
+                            </Form.Group>
+                    </Modal.Body>
 
-                <Modal.Footer>
-                    <Button>
-                        Add Folder
-                    </Button>
-                </Modal.Footer>
+                    <Modal.Footer>
+                        <Button type="submit">
+                            Add Folder
+                        </Button>
+                    </Modal.Footer>
+                </Form>   
             </Modal>
         </>
     )
