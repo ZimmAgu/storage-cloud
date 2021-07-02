8037e1b959c399d4a6755e01d8c4f6b45ab2cd5f
Zim Agu <zimmagu@gmail.com>
2021-07-02 02:52:25 -0700

    Switch statement for the select folder action added

--- src/StorageCloud/FolderUseLogic.js
+++ src/StorageCloud/FolderUseLogic.js
@@ -1,13 +1,23 @@
 import React, { useEffect, useReducer } from 'react'
 
 
-const COMMAND = {
+const ACTION = {
     SELECTFOLDER: 'SelectFolder'
 }
 
 
-function reducer () {
-    
+function reducer (state, { type, payload }) {
+    switch (type) {
+        case ACTION.SELECTFOLDER: // When a folder is selected, the current state of the folder and folder Id are changed to the select folder
+            return {
+                folder: payload.folder,
+                folderId: payload.folderId,
+                childFolders: [],
+                childFiles: []
+            }
+        default: // Returns the current state if you use an ACTION that doesn't exist
+            return state
+    }
 }
 
 
@@ -19,12 +29,12 @@ function FolderUseLogic (folder = null, folderId = null) {
         childFiles: []
     })
 
-    useEffect (() => { 
+    useEffect (() => { // Changes the state of the folder & folder id
         dispatch({ 
-            type: COMMAND.SELECTFOLDER,     
-            payload: { folder,  folderId}   
+            type: ACTION.SELECTFOLDER,     
+            payload: { folder,  folderId }   
         })
-    }, [folder, folderId]) // Runs any times the folder id or the folder changes, the initial states of the folder will be reset
+    }, [folder, folderId]) // Runs any times the folder id or the folder changes
 
 }
 
