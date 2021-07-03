9d894ee9c2035619735b2fa92068823f18589034
Zim Agu <zimmagu@gmail.com>
2021-07-03 10:08:10 -0700

    Child folders now have links & icons

--- src/StorageCloud/Folder.js
+++ src/StorageCloud/Folder.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFolder } from '@fortawesome/free-solid-svg-icons'
+
 
 function Folder({folder}) {
     return (
         <>
-            {folder.name}
+            <Link to='./'>
+                <FontAwesomeIcon icon={faFolder} style={{ marginRight: "0.5em" }}/>
+                {folder.name}
+            </Link>
         </>
     )
 }
