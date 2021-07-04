c0727abbf210c774ca607b40739e2a25b762e3e7
Zim Agu <zimmagu@gmail.com>
2021-07-03 17:10:09 -0700

    Breadcrumbs are now fully functional

--- src/Folders/Breadcrumbs.js
+++ src/Folders/Breadcrumbs.js
@@ -1,9 +1,14 @@
+// React Imports
 import React from 'react'
 import Breadcrumb from 'react-bootstrap/Breadcrumb'
+import { Link } from 'react-router-dom'
+
+// Folders Imports
 import { RootFolder } from './FolderUseLogic'
 
+
 function Breadcrumbs({ currentFolder }) {
-    let folderPath = (currentFolder === RootFolder) ? [] : [RootFolder]
+    let folderPath = (currentFolder === RootFolder) ? [] : [RootFolder] // If the current folder is the root folder, then the folder path is an empty array, otherwise the folder path starts with the Root Folder
 
     if (currentFolder) { // If you have a current folder this will set the path of the folder
         folderPath = [...folderPath, ...currentFolder.path]
@@ -18,8 +23,11 @@ function Breadcrumbs({ currentFolder }) {
                     key={folder.id}
                     style={{maxWidth: '10em'}} 
                 >
-                    {folder.name}
+                    <Link to={folder.id ? `/folder/${folder.id}` : '/'}> 
+                        {folder.name}
+                    </Link>
                 </Breadcrumb.Item>
+                
             ))}  
             {currentFolder && ( 
                 <Breadcrumb.Item 
